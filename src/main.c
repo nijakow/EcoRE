9f62bb059cd5ea4da5809952f10969428c6e5dbf
nijakow <79372954+nijakow@users.noreply.github.com>
2021-06-04 19:49:40 +0200

    Added src/ directory, main.c, Makefile and .gitignore

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+void Eco_Banner()
+{
+    printf("\n");
+    printf("    The Ecosphere\n");
+    printf("    Eco Runtime Environment\n");
+    printf("\n");
+    printf("\n");
+}
+
+
+int main(int argc, char *argv[])
+{
+    Eco_Banner();
+    return 0;
+}
+
