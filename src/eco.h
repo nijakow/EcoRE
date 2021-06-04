3698dd8798edf74b63aaaaf09660bac6563ffbe8
nijakow <79372954+nijakow@users.noreply.github.com>
2021-06-04 20:04:56 +0200

    Added eco.h

---
+++ src/eco.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+
+/*
+ *
+ *    T y p e   D e c l a r a t i o n s
+ *
+ */
+
+typedef uint8_t   u8;
+typedef uint16_t  u16;
+typedef uint32_t  u32;
+
+typedef int8_t    i8;
+typedef int16_t   i16;
+typedef int32_t   i32;
+
+typedef float     f32;
+
+typedef i32       Eco_Integer;
+typedef f32       Eco_Floating;
+
+
+/*
+ *
+ *    O b j e c t   a n d   T y p e   D e c l a r a t i o n s
+ *
+ */
+
+struct Eco_Object;
+struct Eco_Type;
+
+extern struct Eco_Type* Eco_Type_INTEGER;
+extern struct Eco_Type* Eco_Type_FLOATING;
+
+
+/*
+ *
+ *    u n i o n   E c o _ V a l u e
+ *
+ */
+
+typedef union Eco_Value
+{
+    struct Eco_Object*  pointer;
+    Eco_Integer         integer;
+    Eco_Floating        floating;
+} Eco_Value;
+
+
+/*
+ *
+ *    s t r u c t  E c o _ A n y
+ *
+ */
+
+typedef struct Eco_Any
+{
+    Eco_Value         value;
+    struct Eco_Type*  type;
+} Eco_Any;
+
+
+static inline void Eco_Any_AssignPointer(Eco_Any* any, struct Eco_Object* object, struct Eco_Type* type)
+{
+    any->value.pointer = object;
+    any->type          = type;
+}
+
+static inline void Eco_Any_AssignInteger(Eco_Any* any, Eco_Integer integer)
+{
+    any->value.integer = integer;
+    any->type          = Eco_Type_INTEGER;
+}
+
+static inline void Eco_Any_AssignFloating(Eco_Any* any, Eco_Floating floating)
+{
+    any->value.floating = floating;
+    any->type           = Eco_Type_FLOATING;
+}
+
