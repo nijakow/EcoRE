dc397f9f40a37f4bc00509dbbc82cb95b5344dd5
nijakow <79372954+nijakow@users.noreply.github.com>
2021-06-06 15:30:44 +0200

    Implemented Eco_Fiber_Enter(...)

--- src/vm/code.h
+++ src/vm/code.h
@@ -4,6 +4,11 @@
 
 struct Eco_Code
 {
+    unsigned int  register_count;
+    unsigned int  dynamics_count;
+
+    unsigned int  arg_count;
+
     u8*           bytecodes;
     
     unsigned int  constant_count;
--- src/vm/fiber.c
+++ src/vm/fiber.c
@@ -1,28 +1,35 @@
 #include "fiber.h"
 
 #include "bytecodes.h"
+#include "code.h"
 #include "send.h"
 
 
-struct Eco_Frame* Eco_Fiber_PushFrame(struct Eco_Fiber* fiber, unsigned int register_count)
+struct Eco_Frame* Eco_Fiber_PushFrame(struct Eco_Fiber*        fiber,
+                                      unsigned int             register_count,
+                                      unsigned int             dynamics_count,
+                                      struct Eco_Environment*  link)
 {
-    unsigned int      delta;
-    struct Eco_Frame* new_frame;
+    unsigned int       delta;
+    struct Eco_Frame*  new_frame;
 
     delta = sizeof(struct Eco_Frame) * Eco_Fiber_Top(fiber)->register_count * sizeof(Eco_Any); 
 
     fiber->stack_size += delta;
 
     new_frame = Eco_Fiber_Top(fiber);
+
     new_frame->delta = delta;
     new_frame->register_count = register_count;
+
+    /* TODO: Allocate dynamics */
 
     return new_frame;
 }
 
 void Eco_Fiber_PopFrame(struct Eco_Fiber* fiber)
 {
-    struct Eco_Frame* frame;
+    struct Eco_Frame*  frame;
     
     frame = Eco_Fiber_Top(fiber);
 
@@ -34,9 +41,38 @@ void Eco_Fiber_PopFrame(struct Eco_Fiber* fiber)
 }
 
 
+bool Eco_Fiber_Enter(struct Eco_Fiber*        fiber,
+                     struct Eco_Message*      message,
+                     struct Eco_Code*         code,
+                     struct Eco_Environment*  link)
+{
+    unsigned int       i;
+    struct Eco_Frame*  frame;
+
+    if (code->arg_count != (message->arg_count + 1)) {  /* The +1 stands for the SELF value */
+        /* TODO: Error */
+        return false;
+    }
+
+    frame = Eco_Fiber_PushFrame(fiber, code->register_count, code->dynamics_count, link);
+
+    frame->self        = message->arg_location[0];
+
+    frame->code        = code;
+    frame->instruction = code->bytecodes;
+
+    for (i = 0; i < code->arg_count; i++)
+    {
+        frame->registers[i] = message->arg_location[i + 1];
+    }
+
+    return true;
+}
+
+
 void Eco_Fiber_Run(struct Eco_Fiber* fiber)
 {
-    struct Eco_Frame* top;
+    struct Eco_Frame*  top;
 
     top = Eco_Fiber_Top(fiber);
 
@@ -71,12 +107,19 @@ void Eco_Fiber_Run(struct Eco_Fiber* fiber)
                 break;
             }
             case Eco_Bytecode_SEND: {
-                struct Eco_Message message;
+                struct Eco_Message  message;
+
                 message.arg_location = &(top->registers[Eco_Frame_NextU8(top)]);
                 message.arg_count    = Eco_Frame_NextU8(top);
                 message.key          = Eco_Any_AsPointer(Eco_Frame_NextConstant(top));
                 message.fiber        = fiber;
-                /* TODO: Call Eco_Send(...) */
+
+                if (!Eco_Send(&message, message.arg_location)) {
+                    top = Eco_Fiber_Top(fiber); /* TODO: Do the "slow dispatch" code */
+                } else {
+                    /* TODO: Error */
+                }
+
                 break;
             }
             default: {
