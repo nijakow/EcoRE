7377be1a8032f35d2631bb9051d621a972f8ac60
nijakow <79372954+nijakow@users.noreply.github.com>
2021-06-08 12:08:27 +0200

    Extended closure.c

--- src/objects/closure.c
+++ src/objects/closure.c
@@ -1,7 +1,23 @@
 #include "closure.h"
 
+#include "type.h"
 #include "../vm/memory.h"
 #include "../vm/environment.h"
+
+
+void Eco_Closure_MarkSlots(struct Eco_GC_State* state, struct Eco_Closure* closure)
+{
+    /* TODO */
+}
+
+void Eco_Closure_Del(struct Eco_Closure* closure)
+{
+    Eco_Environment_Decr(closure->environment);
+}
+
+
+TYPE_DEFINITION(Eco_Closure, NULL, Eco_Closure_MarkSlots, Eco_Closure_Del, 0, {});
+
 
 struct Eco_Closure* Eco_Closure_New(struct Eco_Code* code, struct Eco_Environment* environment)
 {
