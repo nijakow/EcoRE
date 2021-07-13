3cb8904c669b32a251872473d7bd279e5b63c773
nijakow <79372954+nijakow@users.noreply.github.com>
2021-07-13 18:07:26 +0200

    Moved Eco_EConnect_Builtin_GetKey(...) to builtins/objects/key.c

--- src/ecore/io/econnect/reader/builtins/builtins.c
+++ src/ecore/io/econnect/reader/builtins/builtins.c
@@ -1,34 +1,11 @@
 #include "builtins.h"
 
-#include "builtins_common_header.h"
 #include "protocol.h"
 #include "id.h"
 #include "flow.h"
+#include "objects/key.h"
 
-#include "../parser.h"
-
-#include "../../../../objects/key.h"
-
-
-bool Eco_EConnect_Builtin_GetKey(struct Eco_EConnect_Reader* reader,
-                                 struct Eco_EConnect_Result* result)
-{
-    unsigned int     id;
-    struct Eco_Key*  key;
-
-    id  = Eco_EConnect_ParseUInt(&reader->stream);
-    key = Eco_EConnect_ParseKey(&reader->stream);
-
-    if (key != NULL) {
-        Eco_EConnect_Instance_OptionallyBindObject(reader->instance, (struct Eco_Object*) key, id);
-        Eco_EConnect_Result_Create_Object(result, (struct Eco_Object*) key);
-    } else {
-        Eco_EConnect_Result_Create_Error(result, Eco_EConnect_ErrorType_KEY_NOT_FOUND);
-    }
-
-    return true;
-}
-
+#include "../callback.h"
 
 
 void Eco_EConnect_InitReaderBuiltins()
