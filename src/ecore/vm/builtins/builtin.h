cab8517fe64c281e6574258ad24a1a576ac00d2e
nijakow <79372954+nijakow@users.noreply.github.com>
2021-07-30 00:28:53 +0200

    Added argchecking to vm/builtins/builtin.h

--- src/ecore/vm/builtins/builtin.h
+++ src/ecore/vm/builtins/builtin.h
@@ -3,10 +3,27 @@
 
 #include "../../eco.h"
 
-struct Eco_Fiber;
+#include "../fiber.h"
+
 
 typedef bool (*Eco_Builtin)(struct Eco_Fiber*, unsigned int);
 
-extern bool Eco_VM_Builtin_Trap(struct Eco_Fiber*, unsigned int);
+bool Eco_VM_Builtin_Trap(struct Eco_Fiber*, unsigned int);
+
+
+#define ECO_VM_BUILTIN_INFINITE_ARGS 0xffffffff
+
+static inline bool Eco_VM_Builtin_Tool_ArgExpect(struct Eco_Fiber* fiber,
+                                   unsigned int args,
+                                   unsigned int minargs,
+                                   unsigned int maxargs)
+{
+    if (args < minargs || (args > maxargs)) {
+        Eco_Fiber_SetState(fiber, Eco_Fiber_State_ERROR_ARGERROR);
+        return false;
+    } else {
+        return true;
+    }
+}
 
 #endif
