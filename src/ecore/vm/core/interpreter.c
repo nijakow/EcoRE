ec676027566e9a8494603031e04e3289e7f6eefd
nijakow <79372954+nijakow@users.noreply.github.com>
2021-08-03 11:02:37 +0200

    Renamed ecore/vm/core/bytecodes_dispatch.c to interpreter_dispatch.h

--- src/ecore/vm/core/interpreter.c
+++ src/ecore/vm/core/interpreter.c
@@ -46,23 +46,6 @@ bool Eco_Fiber_Enter(struct Eco_Fiber*  fiber,
 }
 
 
-#define NEXT_U8()             *(instruction++)
-static inline u16 CONSTRUCT_U16(u8** ptr) { u16 v = *((u16*) *ptr); *ptr += 2; return v; }
-#define NEXT_U16()            CONSTRUCT_U16(&instruction)
-#define NEXT_CONSTANT()       &top->code->constants[NEXT_U16()]
-#define TARGET(T)             dispatch_label_ ## T:
-#define DEFAULT_TARGET()      /* TARGET(DEFAULT) */
-#define DISPATCH(B)           goto *DISPATCH_TABLE[B];
-#define ULTRAFAST_DISPATCH()  DISPATCH(NEXT_U8())
-#define FAST_DISPATCH()       goto short_retry
-#define SLOW_DISPATCH()       goto long_retry
-
-#define FAST_PUSH(V)               { Eco_Any_AssignAny((Eco_Any*) sp, V); sp += sizeof(Eco_Any); }
-#define FAST_POP(V)                { sp -= sizeof(Eco_Any); Eco_Any_AssignAny(V, (Eco_Any*) sp); }
-#define FAST_DROP()                { sp -= sizeof(Eco_Any); }
-#define FAST_NTH(N)                ((Eco_Any*) (sp - (N) * sizeof(Eco_Any)))
-#define FAST_DUP()                 { FAST_PUSH(FAST_NTH(1)); }
-
 void Eco_Fiber_Run(struct Eco_Fiber* fiber)
 {
     u8*                instruction;
@@ -71,7 +54,7 @@ void Eco_Fiber_Run(struct Eco_Fiber* fiber)
     struct Eco_Frame*  top;
     struct Eco_Frame*  bottom;
 
-#include "bytecodes_dispatch.c"
+#include "interpreter_dispatch.h"
 
   long_retry:
     top         = Eco_Fiber_Top(fiber);
---
+++ src/ecore/vm/core/interpreter_dispatch.h
@@ -0,0 +1,39 @@
+
+#define DEFDISPATCH(N)  [Eco_Bytecode_ ## N] = &&dispatch_label_ ## N
+
+static void* DISPATCH_TABLE[] = {
+    DEFDISPATCH(NOOP),
+    DEFDISPATCH(CONST),
+    DEFDISPATCH(PUSHC),
+    DEFDISPATCH(PUSH),
+    DEFDISPATCH(POP),
+    DEFDISPATCH(DROP),
+    DEFDISPATCH(DUP),
+    DEFDISPATCH(R2R),
+    DEFDISPATCH(R2L),
+    DEFDISPATCH(L2R),
+    DEFDISPATCH(BUILTIN),
+    DEFDISPATCH(SEND),
+    DEFDISPATCH(ASSIGN),
+    DEFDISPATCH(RETURN),
+    DEFDISPATCH(MAKE_CLOSURE),
+};
+
+
+#define NEXT_U8()             *(instruction++)
+inline u16 CONSTRUCT_U16(u8** ptr) { u16 v = *((u16*) *ptr); *ptr += 2; return v; }
+#define NEXT_U16()            CONSTRUCT_U16(&instruction)
+#define NEXT_CONSTANT()       &top->code->constants[NEXT_U16()]
+#define TARGET(T)             dispatch_label_ ## T:
+#define DEFAULT_TARGET()      /* TARGET(DEFAULT) */
+#define DISPATCH(B)           goto *DISPATCH_TABLE[B];
+#define ULTRAFAST_DISPATCH()  DISPATCH(NEXT_U8())
+#define FAST_DISPATCH()       goto short_retry
+#define SLOW_DISPATCH()       goto long_retry
+
+#define FAST_PUSH(V)               { Eco_Any_AssignAny((Eco_Any*) sp, V); sp += sizeof(Eco_Any); }
+#define FAST_POP(V)                { sp -= sizeof(Eco_Any); Eco_Any_AssignAny(V, (Eco_Any*) sp); }
+#define FAST_DROP()                { sp -= sizeof(Eco_Any); }
+#define FAST_NTH(N)                ((Eco_Any*) (sp - (N) * sizeof(Eco_Any)))
+#define FAST_DUP()                 { FAST_PUSH(FAST_NTH(1)); }
+
