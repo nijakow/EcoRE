359a6cd4d92ab1e3a2faa767701cf4061684476c
nijakow <79372954+nijakow@users.noreply.github.com>
2021-09-20 23:58:00 +0200

    Fixed a typo in ecosphere.compiler.Compiler.__init__

--- src/bootstrap/py2/ecosphere/compiler/__init__.py
+++ src/bootstrap/py2/ecosphere/compiler/__init__.py
@@ -3,5 +3,5 @@ import ecosphere.compiler.scopes
 class Compiler:
 
     def __init__(self, ast, parent_env=None):
-        self._environment = ecosphere.compiler.scopes.Environment(parent_env)
+        self._environment = ecosphere.compiler.scopes.RootEnvironment(parent_env)
 
