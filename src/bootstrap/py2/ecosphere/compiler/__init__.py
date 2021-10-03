b554ecfbe040d60371043e63ace313d3adcd5a13
nijakow <79372954+nijakow@users.noreply.github.com>
2021-10-03 13:36:48 +0200

    Added compilation function to ecosphere.compiler

--- src/bootstrap/py2/ecosphere/compiler/__init__.py
+++ src/bootstrap/py2/ecosphere/compiler/__init__.py
@@ -1,7 +1,16 @@
 import ecosphere.compiler.scopes
+import ecosphere.compiler.visitors
+import ecosphere.compiler.codegen
 
 class Compiler:
 
     def __init__(self, ast, parent_env=None):
         self._environment = ecosphere.compiler.scopes.RootEnvironment(parent_env)
+
+def compile_ast(the_ast, parameters=list()):
+    environment = ecosphere.compiler.scopes.RootEnvironment()
+    writer = ecosphere.compiler.codegen.CodeWriter()
+    codegen = ecosphere.compiler.codegen.CodeGenerator(writer)
+    visitor = ecosphere.compiler.visitors.ASTVisitor(codegen, environment)
+    the_ast.accept(visitor)
 
