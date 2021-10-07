ca3596686c625a8f488661f2f20c9008b2386625
nijakow <79372954+nijakow@users.noreply.github.com>
2021-10-07 15:00:36 +0200

    Implemented evaluation functions for ecosphere.objects in the bootstrapper

--- src/bootstrap/py2/ecosphere/objects/base.py
+++ src/bootstrap/py2/ecosphere/objects/base.py
@@ -1,5 +1,8 @@
 
 class EcoObject:
 
+    def lookup_key(self, key):
+        return None
+
     def __init__(self):
         pass
--- src/bootstrap/py2/ecosphere/objects/plain.py
+++ src/bootstrap/py2/ecosphere/objects/plain.py
@@ -11,15 +11,30 @@ class EcoValueSlot(EcoSlot):
 
     def set_value(self, value):
         self._value = value
+        callbacks = self._callbacks
+        self._callbacks = None
+        for e in callbacks:
+            e(self._value)
+    
+    def evaluate(self, the_subject, the_environment, the_callback, args=None):
+        if self._callbacks is None:
+            the_callback(self._value) # TODO: Wait for the value, in case it wasn't set yet
+        else:
+            self._callbacks.append(the_callback)
 
     def __init__(self, name, is_inherited, is_part):
         super().__init__(name)
         self._is_inherited = is_inherited
         self._is_part = is_part
         self._value = None
+        self._callbacks = list()
 
 
 class EcoCodeSlot(EcoSlot):
+
+    def evaluate(self, the_subject, the_environment, the_callback, args=list()):
+        # TODO, XXX: Handle args, create a new environment
+        self._code.get_ast().evaluate(the_subject, the_environment, the_callback)
 
     def __init__(self, name, code):
         super().__init__(name)
@@ -33,6 +48,9 @@ class EcoPlainObject(ecosphere.objects.base.EcoObject):
             if s.get_name() == key:
                 return s
         return None
+    
+    def lookup_key(self, key):
+        return self.find_slot_by_name(key)
     
     def add_slot(self, slot):
         self._slots.append(slot)
