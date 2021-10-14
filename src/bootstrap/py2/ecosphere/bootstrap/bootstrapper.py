7965a5d68ec3d14afc8c1b7537db5ba50939f0aa
nijakow <79372954+nijakow@users.noreply.github.com>
2021-10-14 11:12:21 +0200

    Added a main function to the bootstrapper

--- src/bootstrap/py2/ecosphere/bootstrap/bootstrapper.py
+++ src/bootstrap/py2/ecosphere/bootstrap/bootstrapper.py
@@ -1,3 +1,4 @@
+import sys
 import os.path
 import pathlib
 
@@ -20,6 +21,15 @@ class FileLoader:
         p = ecosphere.parser.parser.Parser(t)
         expressions = p.parse_expressions(ecosphere.parser.tokenizer.TokenType.EOF)
         return expressions
+    
+    def evaluate(self):
+        expressions = self.parse_expressions()
+        holder = dict()
+        def callback(value):
+            holder['value'] = value
+        for expr in expressions:
+            expr.evaluate(None, self, the_callback)
+        return holder['value']
 
     def __init__(self, shared_info, path):
         self._shared_info = shared_info
@@ -40,43 +50,55 @@ class SharedBootstrappingInfo:
         self._loaded_files = dict()
 
 
-def test(text):
-    s = ecosphere.parser.stream.StringStream(text)
-    t = ecosphere.parser.tokenizer.Tokenizer(s)
-    p = ecosphere.parser.parser.Parser(t)
-    expressions = p.parse_expressions(ecosphere.parser.tokenizer.TokenType.EOF)
-    holder = dict()
-    def the_callback(value):
-        holder['value'] = value
-    for expression in expressions:
-        expression.evaluate(None, None, the_callback)
-    value = holder['value']
-    print('Result:', value)
+def main(srcfile, binfile):
+    shared = SharedBootstrappingInfo()
+    loader = shared.load(srcfile)
+    result = loader.evaluate()
     serializer = ecosphere.econnect.Serializer()
-    serializer.write_object(value)
-    print(serializer.finish())
-
+    serializer.write_object(result)
+    print(serializer.finish())  # TODO: Write to file
 
 if __name__ == '__main__':
-    test('''
-    \{
-        "This is a comment!"
-        x = 5.
-        [int] y = 101.
-        with z = 7.
-        with* zz = 42.
-        with* [int] zzz = 42.
-        [any] routine => hey.
-        square: [int] x => x * x.
-        [int] f([int] x, [int] y) => (x * x) + (2 * y).
-        test => (
-            hello world.
-            this is a test.
-            [ :y => y ].
-            [ :x => y ].
-            [ :x => x <- a ].
-            [ | x <- 5 | x ].
-            y <- a.
-        ).
-    \}.
-    ''')
+    main(sys.argv[1], sys.argv[2])
+
+
+#def test(text):
+#    s = ecosphere.parser.stream.StringStream(text)
+#    t = ecosphere.parser.tokenizer.Tokenizer(s)
+#    p = ecosphere.parser.parser.Parser(t)
+#    expressions = p.parse_expressions(ecosphere.parser.tokenizer.TokenType.EOF)
+#    holder = dict()
+#    def the_callback(value):
+#        holder['value'] = value
+#    for expression in expressions:
+#        expression.evaluate(None, None, the_callback)
+#    value = holder['value']
+#    print('Result:', value)
+#    serializer = ecosphere.econnect.Serializer()
+#    serializer.write_object(value)
+#    print(serializer.finish())
+
+
+#if __name__ == '__main__':
+#    test('''
+#    \{
+#        "This is a comment!"
+#        x = 5.
+#        [int] y = 101.
+#        with z = 7.
+#        with* zz = 42.
+#        with* [int] zzz = 42.
+#        [any] routine => hey.
+#        square: [int] x => x * x.
+#        [int] f([int] x, [int] y) => (x * x) + (2 * y).
+#        test => (
+#            hello world.
+#            this is a test.
+#            [ :y => y ].
+#            [ :x => y ].
+#            [ :x => x <- a ].
+#            [ | x <- 5 | x ].
+#            y <- a.
+#        ).
+#    \}.
+#    ''')
