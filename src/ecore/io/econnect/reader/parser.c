dd59a012ea04988177fe063a432015e38317f6f2
nijakow <79372954+nijakow@users.noreply.github.com>
2021-10-17 18:01:17 +0200

    Fixed a bug found using valgrind: ParseBytes(...) does zero termination of strings, bad for bytecode

--- src/ecore/io/econnect/reader/parser.c
+++ src/ecore/io/econnect/reader/parser.c
@@ -33,6 +33,18 @@ void Eco_EConnect_ParseBytes(struct Eco_IO_ByteInputStream* stream,
         buffer++;
         count--;
     }
+}
+
+void Eco_EConnect_ParseString(struct Eco_IO_ByteInputStream* stream,
+                              char* buffer,
+                              unsigned int count)
+{
+    while (count > 0)
+    {
+        *buffer = Eco_IO_ByteInputStream_Read(stream);
+        buffer++;
+        count--;
+    }
     *buffer = '\0';
 }
 
@@ -43,7 +55,7 @@ struct Eco_Key* Eco_EConnect_ParseKey(struct Eco_IO_ByteInputStream* stream)
     bytes = Eco_EConnect_ParseUInt(stream);
 
     char          buffer[bytes + 1];
-    Eco_EConnect_ParseBytes(stream, buffer, bytes);
+    Eco_EConnect_ParseString(stream, buffer, bytes);
 
     return Eco_Key_Find(buffer);
 }
--- src/ecore/io/econnect/reader/parser.h
+++ src/ecore/io/econnect/reader/parser.h
@@ -8,6 +8,7 @@
 unsigned char Eco_EConnect_ParseByte(struct Eco_IO_ByteInputStream*);
 unsigned int Eco_EConnect_ParseUInt(struct Eco_IO_ByteInputStream*);
 void Eco_EConnect_ParseBytes(struct Eco_IO_ByteInputStream*, char*, unsigned int);
+void Eco_EConnect_ParseString(struct Eco_IO_ByteInputStream*, char*, unsigned int);
 struct Eco_Key* Eco_EConnect_ParseKey(struct Eco_IO_ByteInputStream*);
 
 #endif
