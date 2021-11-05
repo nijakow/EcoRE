3356f3c486c9de23b3ac1851b66aabfc3607674c
nijakow <79372954+nijakow@users.noreply.github.com>
2021-11-05 21:36:01 +0100

    Added extra.h

---
+++ src/ecore/extra.h
@@ -0,0 +1,18 @@
+#ifndef ECO_EXTRA_H
+#define ECO_EXTRA_H
+
+#include <ecore/eco.h>
+#include <ecore/objects/base/object.h>
+#include <ecore/objects/base/type.h>
+
+
+extern struct Eco_Type*  Eco_Integer_TYPE;
+
+static inline struct Eco_Type* Eco_Any_GetType(Eco_Any* any)
+{
+    if (Eco_Any_IsPointer(any)) return Eco_Any_AsPointer(any)->type;
+    else if (Eco_Any_IsInteger(any)) return Eco_Integer_TYPE;
+    else return NULL;
+}
+
+#endif
