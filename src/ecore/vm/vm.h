061b2906a0675fbf0aa381076e010f65e07f23b4
nijakow <79372954+nijakow@users.noreply.github.com>
2021-11-15 23:01:15 +0100

    Refactored src/ecore/vm/vm.h

--- src/ecore/vm/vm.h
+++ src/ecore/vm/vm.h
@@ -32,9 +32,9 @@ void Eco_VM_Destroy(struct Eco_VM*);
 void Eco_VM_Mark(struct Eco_GC_State*, struct Eco_VM*);
 void Eco_VM_FreeAll(struct Eco_VM*);
 
-struct Eco_Fiber* Eco_VM_SpawnThunk(struct Eco_VM*, struct Eco_Code*);
 void Eco_VM_Run(struct Eco_VM*);
 
+struct Eco_Fiber* Eco_VM_SpawnThunk(struct Eco_VM*, struct Eco_Code*);
 bool Eco_VM_LoadImageFromFile(struct Eco_VM*, const char*);
 
 #endif
