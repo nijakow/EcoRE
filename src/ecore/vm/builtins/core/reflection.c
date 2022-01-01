14c738768e42780c120e05c9972cdc0c53fd2cb2
nijakow <79372954+nijakow@users.noreply.github.com>
2022-01-01 17:34:46 +0100

    Fixed an argument count error in Eco_VM_Builtin_GetType(...)

--- src/ecore/vm/builtins/core/reflection.c
+++ src/ecore/vm/builtins/core/reflection.c
@@ -5,7 +5,7 @@
 
 bool Eco_VM_Builtin_GetType(struct Eco_Fiber* fiber, unsigned int args)
 {
-    if (!Eco_VM_Builtin_Tool_ArgExpect(fiber, args, 2, 2))
+    if (!Eco_VM_Builtin_Tool_ArgExpect(fiber, args, 1, 1))
         return false;
     Eco_Any_AssignPointer(Eco_Fiber_Peek(fiber),
                           (struct Eco_Object*) Eco_Any_GetType(Eco_Fiber_Peek(fiber)));
