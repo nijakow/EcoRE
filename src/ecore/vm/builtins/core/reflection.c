4a0b959a4a1425113a9a29521c934806032e51e3
enijakow <79372954+nijakow@users.noreply.github.com>
2022-01-04 15:25:41 +0100

    Switched to 'ecosphere.ecore.object.interface.get_entry_info' for getEntryNames

--- src/ecore/vm/builtins/core/reflection.c
+++ src/ecore/vm/builtins/core/reflection.c
@@ -37,31 +37,6 @@ bool Eco_VM_Builtin_GetTypeSlotNames(struct Eco_Fiber* fiber, unsigned int args)
     return true;
 }
 
-bool Eco_VM_Builtin_InterfaceGetEntryNames(struct Eco_Fiber* fiber, unsigned int args)
-{
-    struct Eco_Interface*  interface;
-    struct Eco_Array*      array;
-    Eco_Any                any;
-    unsigned int           index;
-
-    if (!Eco_VM_Builtin_Tool_ArgExpect(fiber, args, 1, 1))
-        return false;
-    
-    Eco_Fiber_Pop(fiber, &any);
-    interface = (struct Eco_Interface*) Eco_Any_AsPointer(&any);
-    
-    array = Eco_Array_New(interface->entry_count);
-
-    for (index = 0; index < interface->entry_count; index++)
-    {
-        Eco_Any_AssignPointer(&any, (struct Eco_Object*) interface->entries[index].key);
-        Eco_Array_Put(array, index, &any);
-    }
-
-    Eco_Any_AssignPointer(&any, (struct Eco_Object*) array);
-    Eco_Fiber_Push(fiber, &any);
-    return true;
-}
 
 bool Eco_VM_Builtin_InterfaceGetEntryInfo(struct Eco_Fiber* fiber, unsigned int args)
 {
--- src/ecore/vm/builtins/core/reflection.h
+++ src/ecore/vm/builtins/core/reflection.h
@@ -6,7 +6,6 @@
 bool Eco_VM_Builtin_GetType(struct Eco_Fiber*, unsigned int);
 bool Eco_VM_Builtin_GetTypeSlotNames(struct Eco_Fiber*, unsigned int);
 
-bool Eco_VM_Builtin_InterfaceGetEntryNames(struct Eco_Fiber*, unsigned int);
 bool Eco_VM_Builtin_InterfaceGetEntryInfo(struct Eco_Fiber*, unsigned int);
 bool Eco_VM_Builtin_InterfaceAddEntry(struct Eco_Fiber*, unsigned int);
 
