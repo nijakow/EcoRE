49e037c284e425506edee8deb9697cb20a3f34c3
nijakow <79372954+nijakow@users.noreply.github.com>
2022-03-06 23:50:37 +0100

    Implemented a directory creation primitive

---
+++ src/ecore/vm/builtins/core/directory.h
@@ -0,0 +1,8 @@
+#ifndef ECO_VM_BUILTINS_CORE_DIRECTORY_H
+#define ECO_VM_BUILTINS_CORE_DIRECTORY_H
+
+#include <ecore/vm/builtins/builtin.h>
+
+bool Eco_VM_Builtin_Directory_Open(struct Eco_Fiber*, unsigned int);
+
+#endif
