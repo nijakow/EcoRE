1fbcc5b6d330b091f03c696f60af08ea6d31d4df
nijakow <79372954+nijakow@users.noreply.github.com>
2022-03-06 23:43:10 +0100

    Implemented Eco_Directory_Send(...)

--- src/ecore/objects/io/directory.c
+++ src/ecore/objects/io/directory.c
@@ -1,7 +1,19 @@
+#include <dirent.h>
+
 #include "directory.h"
 
 #include <ecore/objects/base/type.h>
 #include <ecore/util/memory.h>
+
+
+/*
+ *    D e c l a r a t i o n s
+ */
+
+bool Eco_Directory_Send(struct Eco_Message* message,
+                        struct Eco_SendLink* link,
+                        struct Eco_Object* target,
+                        Eco_Any* self);
 
 
 /*
@@ -16,7 +28,7 @@ void Eco_Directory_Init()
 {
     Eco_TypeCore_Create(&Eco_Directory_TYPECORE, "Eco_Directory");
 
-    Eco_Directory_TYPECORE.send = (Eco_TypeCore_SendFunc) Eco_Object_Send;
+    Eco_Directory_TYPECORE.send = (Eco_TypeCore_SendFunc) Eco_Directory_Send;
     Eco_Directory_TYPECORE.mark = (Eco_TypeCore_MarkFunc) Eco_Directory_Mark;
     Eco_Directory_TYPECORE.del  = (Eco_TypeCore_DelFunc)  Eco_Directory_Del;
 
@@ -39,12 +51,13 @@ struct Eco_Directory* Eco_Directory_New(char* path)
     unsigned int           path_length;
 
     path_length = Eco_Strlen(path);
-    directory   = Eco_Object_New(Eco_Directory_TYPE, sizeof(struct Eco_Directory) + sizeof(char) * path_length);
+    directory   = Eco_Object_New(Eco_Directory_TYPE, sizeof(struct Eco_Directory) + sizeof(char) * (path_length + 1));
 
     if (directory != NULL)
     {
         directory->path_length = path_length;
         Eco_Memcpy(directory->path, path, path_length);
+        directory->path[path_length] = '\0';
     }
 
     return directory;
@@ -58,4 +71,32 @@ void Eco_Directory_Mark(struct Eco_GC_State* state, struct Eco_Directory* direct
 void Eco_Directory_Del(struct Eco_Directory* directory)
 {
     Eco_Object_Del(&directory->_);
+}
+
+
+/*
+ *    T h e   S p i c y   P a r t
+ */
+
+bool Eco_Directory_Send(struct Eco_Message* message,
+                        struct Eco_SendLink* link,
+                        struct Eco_Object* target,
+                        Eco_Any* self)
+{
+    struct Eco_Directory*  directory;
+    DIR*                   d;
+    struct dirent*         dir;
+
+    directory = (struct Eco_Directory*) Eco_Any_AsPointer(self);
+    
+    d = opendir(directory->path);
+    if (d != NULL)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            Eco_Log_Debug("%s\n", dir->d_name);
+        }
+    }
+    closedir(d);
+    return false;
 }
