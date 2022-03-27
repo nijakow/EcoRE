b729a82c9584ed8d8de3e44b6ddba59b86998529
nijakow <79372954+nijakow@users.noreply.github.com>
2022-03-27 14:24:55 +0200

    Added vm and fiber

---
+++ src/ecore/vm/fiber/fiber.h
@@ -0,0 +1,8 @@
+#ifndef ECO_VM_FIBER_FIBER_H
+#define ECO_VM_FIBER_FIBER_H
+
+struct eco_fiber
+{
+};
+
+#endif
---
+++ src/ecore/vm/vm.h
@@ -0,0 +1,8 @@
+#ifndef ECO_VM_VM_H
+#define ECO_VM_VM_H
+
+struct eco_vm
+{
+};
+
+#endif
