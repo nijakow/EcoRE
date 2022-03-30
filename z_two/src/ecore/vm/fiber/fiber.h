6f520457649d96cfdddf5885ded14aca3e278943
nijakow <79372954+nijakow@users.noreply.github.com>
2022-03-31 01:03:06 +0200

    Removed z_two

--- z_two/src/ecore/vm/fiber/fiber.h
+++
@@ -1,8 +0,0 @@
-#ifndef ECO_VM_FIBER_FIBER_H
-#define ECO_VM_FIBER_FIBER_H
-
-struct eco_fiber
-{
-};
-
-#endif
--- z_two/src/ecore/vm/vm.h
+++
@@ -1,8 +0,0 @@
-#ifndef ECO_VM_VM_H
-#define ECO_VM_VM_H
-
-struct eco_vm
-{
-};
-
-#endif
