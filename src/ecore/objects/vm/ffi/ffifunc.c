0d00d9f0f6f7ba7eb820ce76a6a2d378b08da2a4
nijakow <79372954+nijakow@users.noreply.github.com>
2022-04-14 18:15:12 +0200

    Implemented Eco_FFIFunc_Call

--- src/ecore/objects/vm/ffi/ffifunc.c
+++ src/ecore/objects/vm/ffi/ffifunc.c
@@ -1,6 +1,7 @@
 #include "ffifunc.h"
 
 #include <ecore/objects/base/type.h>
+#include <ecore/vm/memory/gc/gc.h>
 
 
 static struct Eco_TypeCore Eco_FFIFunc_TYPECORE;
@@ -35,9 +36,14 @@ struct Eco_FFIFunc* Eco_FFIFunc_New(unsigned int args, struct Eco_FFIType* rtype
 
     if (func != NULL)
     {
+        func->arg_count = args;
 #ifdef ECO_CONFIG_USE_FFI
         for (index = 0; index < args; index++)
+        {
             actual_args[index] = arg_types[index]->type;
+            func->arg_types[index] = arg_types[index];
+        }
+        func->return_type = rtype;
         ffi_prep_cif(&func->cif, FFI_DEFAULT_ABI, args, rtype->type, actual_args);
 #endif
     }
@@ -45,12 +51,65 @@ struct Eco_FFIFunc* Eco_FFIFunc_New(unsigned int args, struct Eco_FFIType* rtype
     return func;
 }
 
-void Eco_FFIFunc_Mark(struct Eco_GC_State* state, struct Eco_FFIFunc* type)
+void Eco_FFIFunc_Mark(struct Eco_GC_State* state, struct Eco_FFIFunc* func)
 {
-    Eco_Object_Mark(state, &type->_);
+    unsigned int  index;
+
+#ifdef ECO_CONFIG_USE_FFI
+    for (index = 0; index < func->arg_count; index++)
+        Eco_GC_State_MarkObject(state, func->arg_types[index]);
+    Eco_GC_State_MarkObject(state, func->return_type);
+#endif
+    Eco_Object_Mark(state, &func->_);
 }
 
-void Eco_FFIFunc_Del(struct Eco_FFIFunc* type)
+void Eco_FFIFunc_Del(struct Eco_FFIFunc* func)
 {
-    Eco_Object_Del(&type->_);
+    Eco_Object_Del(&func->_);
+}
+
+
+unsigned int Eco_FFIFunc_GetArgumentCount(struct Eco_FFIFunc* func)
+{
+    return func->arg_count;
+}
+
+struct Eco_FFIType* Eco_FFIFunc_GetReturnType(struct Eco_FFIFunc* func)
+{
+#ifdef ECO_CONFIG_USE_FFI
+    return func->return_type;
+#else
+    return NULL;
+#endif
+}
+
+struct Eco_FFIType* Eco_FFIFunc_GetArgumentType(struct Eco_FFIFunc* func, unsigned int index)
+{
+#ifdef ECO_CONFIG_USE_FFI
+    if (index >= func->arg_count)
+        return NULL;
+    return func->arg_types[index];
+#else
+    return NULL;
+#endif
+}
+
+bool Eco_FFIFunc_Call(struct Eco_FFIFunc* func, void* function, void* buffer)
+{
+    char*         buffer_ptr;
+    unsigned int  index;
+    void*         args[func->arg_count];
+
+#ifdef ECO_CONFIG_USE_FFI
+    buffer_ptr = buffer;
+    for (index = 0; index < func->arg_count; index++)
+    {
+        args[index] = buffer_ptr;
+        buffer_ptr += Eco_FFIType_SizeofCType(func->arg_types[index]);
+    }
+    ffi_call(&func->cif, function, buffer_ptr, args);
+    return true;
+#else
+    return false;
+#endif
 }
--- src/ecore/objects/vm/ffi/ffifunc.h
+++ src/ecore/objects/vm/ffi/ffifunc.h
@@ -15,17 +15,25 @@ struct Eco_FFIFunc
 {
     struct Eco_Object     _;
 
+    unsigned int          arg_count;
 #ifdef ECO_CONFIG_USE_FFI
     ffi_cif               cif;
+    struct Eco_FFIType*   return_type;
+    struct Eco_FFIType*   arg_types[];
 #endif
 };
 
+void Eco_FFIFunc_Init();
+void Eco_FFIFunc_Terminate();
 
 struct Eco_FFIFunc* Eco_FFIFunc_New(unsigned int, struct Eco_FFIType*, struct Eco_FFIType**);
 void Eco_FFIFunc_Mark(struct Eco_GC_State*, struct Eco_FFIFunc*);
 void Eco_FFIFunc_Del(struct Eco_FFIFunc*);
 
-void Eco_FFIFunc_Init();
-void Eco_FFIFunc_Terminate();
+unsigned int Eco_FFIFunc_GetArgumentCount(struct Eco_FFIFunc*);
+struct Eco_FFIType* Eco_FFIFunc_GetReturnType(struct Eco_FFIFunc*);
+struct Eco_FFIType* Eco_FFIFunc_GetArgumentType(struct Eco_FFIFunc*, unsigned int);
+
+bool Eco_FFIFunc_Call(struct Eco_FFIFunc*, void*, void*);
 
 #endif
