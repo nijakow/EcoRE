fe424d73d67670b3671b63c2d39bb2523b562692
nijakow <79372954+nijakow@users.noreply.github.com>
2022-05-16 13:15:48 +0200

    Allowed struct Eco_Pages to allocate new pages

--- src/ecorevx/core/memory/pages.c
+++ src/ecorevx/core/memory/pages.c
@@ -1,5 +1,19 @@
 #include "pages.h"
 
+static struct Eco_Page* Eco_Pages_AllocateNewPage(struct Eco_Pages* self)
+{
+    struct Eco_Page*  page;
+
+    page = Eco_Page_New();
+
+    if (page != NULL)
+    {
+        Eco_Page_Link(page, &self->page_list);
+    }
+
+    return page;
+}
+
 void Eco_Pages_Create(struct Eco_Pages* self)
 {
     self->page_list = NULL;
