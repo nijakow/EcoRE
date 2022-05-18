ba1e204325fe1aaef5226d603f06bee1b7d9c7c3
nijakow <79372954+nijakow@users.noreply.github.com>
2022-05-18 23:02:33 +0200

    Extended map.c

--- src/ecorevx/core/map/map.c
+++ src/ecorevx/core/map/map.c
@@ -21,7 +21,7 @@ static void Eco_Map_RecomputeInlineSlotOffsets(struct Eco_Map* self)
 }
 
 
-struct Eco_Map*  Eco_Map_New(unsigned int slot_count)
+static struct Eco_Map*  Eco_Map_New(unsigned int slot_count)
 {
     struct Eco_Map*  self;
 
@@ -38,30 +38,90 @@ struct Eco_Map*  Eco_Map_New(unsigned int slot_count)
     return self;
 }
 
-struct Eco_Map*  Eco_Map_CopyWithSlot(struct Eco_Map*      self,
-                                      unsigned int         index,
-                                      struct Eco_SlotDef   slot_def,
-                                      struct Eco_MapSlot** slot_loc)
+
+/*
+ * Update the map's inheritance links and invalidation pointers.
+ * This function must be called after the core structures of the map have been established.
+ */
+static void Eco_Map_Finalize(struct Eco_Map* self)
 {
-    const unsigned int slot_count = self->slot_count + 1;
-    const unsigned int slot_index = Eco_Map_NormalizeIndex(self, index);
+    /*
+     * TODO
+     */
+}
+
+
+static struct Eco_Map*  Eco_Map_CopyWithSlot(struct Eco_Map*      self,
+                                             unsigned int         index,
+                                             struct Eco_SlotDef   slot_def,
+                                             struct Eco_MapSlot** slot_loc)
+{
     struct Eco_Map*    copy;
     unsigned int       i;
 
-    copy = Eco_Map_New(slot_count);
+    /*
+     * Create a new map.
+     */
+    copy = Eco_Map_New(self->slot_count + 1);
 
+    /*
+     * Check if the allocation succeeded.
+     */
     if (copy != NULL)
     {
-        copy->instance_slot_count = self->instance_slot_count;
-        copy->slot_count          = self->slot_count;
+        /*
+         * Copy the slots over, skip the newly created one.
+         */
         for (i = 0; i < copy->slot_count; i++)
         {
-            copy->slots[i] = self->slots[(i < slot_index) ? i : (i + 1)];
+                 if (i  < index) copy->slots[i] = self->slots[i];
+            else if (i == index);
+            else if (i  > index) copy->slots[i] = self->slots[i - 1];
         }
-        copy->slots[index].def = slot_def;
+
         /*
-         * TODO: Update the map pointers inside of the slots!
+         * Copy the slot definition from the argument.
          */
+        copy->slots[index].def = slot_def;
+
+        /*
+         * Set up the indirect return value: A pointer to the slot.
+         */
+        *slot_loc = &copy->slots[index];
+    }
+
+    return copy;
+}
+
+static struct Eco_Map*  Eco_Map_CopyWithoutSlot(struct Eco_Map*      self,
+                                                unsigned int         index,
+                                                struct Eco_SlotDef   slot_def)
+{
+    struct Eco_Map*    copy;
+    unsigned int       i;
+
+    /*
+     * Create a new map.
+     */
+    copy = Eco_Map_New(self->slot_count - 1);
+
+    /*
+     * Check if the allocation succeeded.
+     */
+    if (copy != NULL)
+    {
+        /*
+         * Copy the slots over.
+         */
+        for (i = 0; i < copy->slot_count; i++)
+        {
+            copy->slots[i] = self->slots[(i < index) ? i : (i + 1)];
+        }
+
+        /*
+         * Copy the slot definition from the argument.
+         */
+        copy->slots[index].def = slot_def;
     }
 
     return copy;
@@ -74,8 +134,6 @@ struct Eco_Map*  Eco_Map_AddValueSlot(struct Eco_Map*    self,
 {
     struct Eco_Map*      new_map;
     struct Eco_MapSlot*  new_slot;
-    unsigned int         i;
-    unsigned int         j;
 
     new_map = Eco_Map_CopyWithSlot(self, index, slot_def, &new_slot);
 
@@ -89,9 +147,15 @@ struct Eco_Map*  Eco_Map_AddValueSlot(struct Eco_Map*    self,
         new_slot->body.inlined.referenced_map = (slot_def.flags.is_with) ? new_map : NULL;
 
         /*
-         * 
+         * Recompute the slot offsets. This will insert a value into body.inlined.offset
+         * and update the map's instance size.
          */
         Eco_Map_RecomputeInlineSlotOffsets(new_map);
+
+        /*
+         * Finalize the map. This has to be the last step.
+         */
+        Eco_Map_Finalize(new_map);
     }
 
     return new_map;
@@ -102,16 +166,42 @@ struct Eco_Map*  Eco_Map_AddCodeSlot(struct Eco_Map*    self,
                                      struct Eco_SlotDef slot_def,
                                      struct Eco_Object* code)
 {
-    /*
-     * TODO
-     */
-    return NULL;
+    struct Eco_Map*      new_map;
+    struct Eco_MapSlot*  new_slot;
+
+    new_map = Eco_Map_CopyWithSlot(self, index, slot_def, &new_slot);
+
+    if (new_map != NULL)
+    {
+        /*
+         * Our newly created slot is only rudimentarily initialized. We now
+         * set up its code pointer.
+         */
+        new_slot->body.code.code = code;
+
+        /*
+         * Finalize the map. This has to be the last step.
+         */
+        Eco_Map_Finalize(new_map);
+    }
+
+    return new_map;
 }
 
 struct Eco_Map*  Eco_Map_RemoveSlot(struct Eco_Map* self, int index)
 {
-    /*
-     * TODO
-     */
-    return NULL;
+    struct Eco_Map*      new_map;
+    struct Eco_MapSlot*  new_slot;
+
+    new_map = Eco_Map_CopyWithoutSlot(self, index, slot_def);
+
+    if (new_map != NULL)
+    {
+        /*
+         * Finalize the map. This has to be the last step.
+         */
+        Eco_Map_Finalize(new_map);
+    }
+    
+    return new_map;
 }
