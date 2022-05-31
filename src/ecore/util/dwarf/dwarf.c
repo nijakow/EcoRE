9b02e326b7250354f2d84d0d2a7bb10efa75b8b9
nijakow <79372954+nijakow@users.noreply.github.com>
2022-05-31 21:35:11 +0200

    Extended the DWARF library bindings

--- src/ecore/util/dwarf/dwarf.c
+++ src/ecore/util/dwarf/dwarf.c
@@ -1,5 +1,6 @@
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
 
 #include <ecore/util/libc.h>
 #include <ecore/util/memory.h>
@@ -26,6 +27,31 @@ void Eco_Dwarf_Destroy(struct Eco_Dwarf* dwarf)
     close(dwarf->fd);
 }
 
+struct Eco_DwarfDie* Eco_Dwarf_GetFirstDie(struct Eco_Dwarf* dwarf)
+{
+    struct Eco_DwarfDie*  die;
+    Dwarf_Die             dwarf_die;
+
+    dwarf_next_cu_header(dwarf->debug, NULL, NULL, NULL, NULL, NULL, NULL);
+
+    if (dwarf_siblingof(dwarf->debug, NULL, &dwarf_die, NULL) != DW_DLV_OK)
+        return NULL;
+
+    die = Eco_LibC_Alloc(sizeof(struct Eco_DwarfDie));
+
+    if (die != NULL)
+    {
+        die->session    =  dwarf;
+        die->die        =  dwarf_die;
+        die->parent     =  NULL;
+        die->sibling    =  NULL;
+        die->child      =  NULL;
+        die->prev       =  NULL;    // TODO: List of dwarf entries in session
+    }
+
+    return die;
+}
+
 struct Eco_DwarfDie* Eco_DwarfDie_Sibling(struct Eco_DwarfDie* prev)
 {
     struct Eco_DwarfDie*  die;
@@ -34,7 +60,7 @@ struct Eco_DwarfDie* Eco_DwarfDie_Sibling(struct Eco_DwarfDie* prev)
     if (prev->sibling != NULL)
         return prev->sibling;
 
-    if (dwarf_siblingof(prev->session->debug, prev->die, &dwarf_die, NULL) == DW_DLV_ERROR)
+    if (dwarf_siblingof(prev->session->debug, prev->die, &dwarf_die, NULL) != DW_DLV_OK)
         return NULL;
 
     die = Eco_LibC_Alloc(sizeof(struct Eco_DwarfDie));
@@ -61,7 +87,7 @@ struct Eco_DwarfDie* Eco_DwarfDie_Child(struct Eco_DwarfDie* parent)
     if (parent->child != NULL)
         return parent->child;
 
-    if (dwarf_child(parent->die, &dwarf_die, NULL) == DW_DLV_ERROR)
+    if (dwarf_child(parent->die, &dwarf_die, NULL) != DW_DLV_OK)
         return NULL;
 
     die = Eco_LibC_Alloc(sizeof(struct Eco_DwarfDie));
@@ -99,12 +125,65 @@ bool Eco_DwarfDie_Is(struct Eco_DwarfDie* die, const char* name)
     return Eco_StrEq(name, tag_name);
 }
 
+bool Eco_DwarfDie_Name(struct Eco_DwarfDie* die, char* dest, Eco_Size_t size)
+{
+    Dwarf_Error  error;
+    Dwarf_Half   tag;
+    const char*  tag_name;
+
+    tag   = 0;
+    error = 0;
+    if (dwarf_tag(die->die, &tag, &error) != DW_DLV_OK)
+        return false;
+    dwarf_get_TAG_name(tag, &tag_name);
+    Eco_Strncpy(dest, tag_name, size);
+    return true;
+}
+
 bool Eco_DwarfDie_AttrName(struct Eco_DwarfDie* die, char* dest, Eco_Size_t size)
 {
     char*  die_name;
 
-    dwarf_diename(die->die, &die_name, NULL);
+    if (dwarf_diename(die->die, &die_name, NULL) != DW_DLV_OK)
+        return false;
     Eco_Strncpy(dest, die_name, size);
     dwarf_dealloc(die->session->debug, die_name, DW_DLA_STRING);
     return true; /* TODO: Check success */
+}
+
+static void Eco_DwarfDie_PrintRecursively(struct Eco_DwarfDie* die, int depth)
+{
+    struct Eco_DwarfDie*  other;
+    char                  buffer[1024];
+
+    for (int x = 0; x < depth; x++)
+        printf(" ");
+    if (Eco_DwarfDie_Name(die, buffer, sizeof(buffer)))
+        printf("%s", buffer);
+    else
+        printf("???");
+    if (Eco_DwarfDie_AttrName(die, buffer, sizeof(buffer)))
+        printf(": %s", buffer);
+    printf("\n");
+    if ((other = Eco_DwarfDie_Child(die)) != NULL)
+        Eco_DwarfDie_PrintRecursively(other, depth + 1);
+    if ((other = Eco_DwarfDie_Sibling(die)) != NULL)
+        Eco_DwarfDie_PrintRecursively(other, depth);
+}
+
+static void Eco_DwarfDie_Print(struct Eco_DwarfDie* die)
+{
+    Eco_DwarfDie_PrintRecursively(die, 0);
+}
+
+void Eco_Dwarf_Test(const char* path)
+{
+    struct Eco_Dwarf      session;
+    struct Eco_DwarfDie*  die;
+
+    Eco_Dwarf_Create(&session, path);
+    die = Eco_Dwarf_GetFirstDie(&session);
+    if (die != NULL)
+        Eco_DwarfDie_Print(die);
+    Eco_Dwarf_Destroy(&session);
 }
--- src/ecore/util/dwarf/dwarf.h
+++ src/ecore/util/dwarf/dwarf.h
@@ -28,12 +28,14 @@ struct Eco_DwarfDie
     struct Eco_DwarfDie** prev;
 };
 
+struct Eco_DwarfDie* Eco_Dwarf_GetFirstDie(struct Eco_Dwarf*);
+
 struct Eco_DwarfDie* Eco_DwarfDie_Sibling(struct Eco_DwarfDie*);
 struct Eco_DwarfDie* Eco_DwarfDie_Child(struct Eco_DwarfDie*);
 void Eco_DwarfDie_Delete(struct Eco_DwarfDie*);
 
 bool Eco_DwarfDie_Is(struct Eco_DwarfDie*, const char*);
-
+bool Eco_DwarfDie_Name(struct Eco_DwarfDie*, char*, Eco_Size_t);
 bool Eco_DwarfDie_AttrName(struct Eco_DwarfDie*, char*, Eco_Size_t);
 bool Eco_DwarfDie_AttrType(struct Eco_DwarfDie*, struct Eco_DwarfDie**);
 bool Eco_DwarfDie_AttrLocation(struct Eco_DwarfDie*, void**);
@@ -41,6 +43,7 @@ bool Eco_DwarfDie_AttrByteSize(struct Eco_DwarfDie*, Eco_Size_t*);
 bool Eco_DwarfDie_AttrExternal(struct Eco_DwarfDie*, bool*);
 // TODO: bool Eco_DwarfDie_AttrEncoding(struct Eco_DwarfDie*, enum Eco_DwarfEncoding);
 
+void Eco_Dwarf_Test(const char*);
 
 #endif
 
