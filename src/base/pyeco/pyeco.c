6ccd65ea5600c39bf502f45556aa4c68e9f9038e
nijakow <79372954+nijakow@users.noreply.github.com>
2023-08-18 11:30:02 +0200

    PyEco refactoring

--- src/base/pyeco/pyeco.c
+++ src/base/pyeco/pyeco.c
@@ -4,16 +4,10 @@ void PyEco_Initialize()
 {
     static bool initialized = false;
 
-    if (initialized) {
-        printf("PyEco already initialized.\n");
-        return;
+    if (!initialized) {
+        Py_Initialize();
+        initialized = true;   
     }
-
-    printf("Initializing...\n");
-    
-    Py_Initialize();
-
-    initialized = true;
 }
 
 void PyEco_Finalize()
@@ -24,9 +18,6 @@ void PyEco_Finalize()
 
 bool PyEco_TypeCheck(PyObject* obj, PyTypeObject* type)
 {
-    printf("Typechecking %p against %p\n", obj, type);
-    printf("Module type is: %p\n", &PyModule_Type);
-    
     if (obj == NULL || type == NULL) {
         return false;
     }
